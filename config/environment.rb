cc5da0ea98f4c2ebd0a5f2c1354d1fcd299950e4
Dmitry Sadovnikov <sadovnikov.js@gmail.com>
2019-06-05 00:49:51 +0300

    change schema_format

--- config/environment.rb
+++ config/environment.rb
@@ -6,5 +6,7 @@ Dotenv.load(".env.#{ENV['SINATRA_ENV']}")
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
+ActiveRecord::Base.schema_format = :sql
+
 require './app/controllers/application_controller'
 require_all 'app'
